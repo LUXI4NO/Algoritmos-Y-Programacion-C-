e8d159c56f68f7a6d13698465bec5722eeb8c48e
Luciano Alvarez <140111840+LUXI4NO@users.noreply.github.com>
2025-05-30 11:25:24 -0300

    #5

---
+++ Proyecto Recursiva #2/Proyecto Recursiva #2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections; 
+
+namespace Proyecto_Herencia__2
+{
+    class Program 
+    {
+        public static void ImprimirPreciosRecursiva(ArrayList precios, int indice)
+        {
+            if (indice >= precios.Count)
+            {
+                return; 
+            }
+
+            double precioActualButaca = (double)precios[indice];
+
+            if (precioActualButaca >= 2500)
+            {
+                Console.WriteLine("Procesando butaca en índice " + indice + " con precio: " + precioActualButaca);
+            }
+            
+            ImprimirPreciosRecursiva(precios, indice + 1);
+        }
+
+        public static void Main(string[] args)
+        {
+            ArrayList preciosButacas = new ArrayList();
+
+            preciosButacas.Add(100000.0);
+            preciosButacas.Add(1500.0);
+            preciosButacas.Add(2501.0);
+            preciosButacas.Add(3000.0); 
+            preciosButacas.Add(2000.0); 
+
+            Console.WriteLine("Precios de butacas (filtrando >= 2500):");
+            
+            ImprimirPreciosRecursiva(preciosButacas, 0);
+
+            Console.WriteLine("\nProceso completado. Presione cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
