04216731b82156d433e2f12e8c732f72f6dde277
Luciano Alvarez <140111840+LUXI4NO@users.noreply.github.com>
2025-05-30 11:24:16 -0300

    #3

---
+++ Proyecto Herencia/Proyecto Herencia/Productos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Producto
+{
+    // Atributos comunes
+    public string Nombre { get; set; }
+    public string Marca { get; set; }
+    public double Precio { get; set; }
+    public int Stock { get; set; }
+
+    // Constructor
+    public Producto(string nombre, string marca, double precio, int stock)
+    {
+        Nombre = nombre;
+        Marca = marca;
+        Precio = precio;
+        Stock = stock;
+    }
+
+    // Método polimórfico
+    public virtual double CalcularPrecio()
+    {
+        return Precio;
+    }
+}
+
+public class ProductoDeOferta : Producto
+{
+    // Atributo adicional
+    public double PorcentajeDeDescuento { get; set; }
+
+    // Constructor
+    public ProductoDeOferta(string nombre, string marca, double precio, int stock, double porcentajeDeDescuento)
+        : base(nombre, marca, precio, stock)
+    {
+        PorcentajeDeDescuento = porcentajeDeDescuento;
+    }
+
+    // Redefinición del método polimórfico
+    public override double CalcularPrecio()
+    {
+        return Precio - (Precio * PorcentajeDeDescuento / 100);
+    }
+}
+
---
+++ Proyecto Herencia/Proyecto Herencia/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Producto normal
+        Producto p1 = new Producto("Notebook", "HP", 200000, 10);
+        Console.WriteLine("Precio de producto normal: " + p1.CalcularPrecio());
+
+        // Producto en oferta
+        Producto p2 = new ProductoDeOferta("Mouse", "Logitech", 5000, 20, 10); 
+        Console.WriteLine("Precio de producto en oferta: " + p2.CalcularPrecio());
+
+        Console.ReadKey();
+    }
+}
