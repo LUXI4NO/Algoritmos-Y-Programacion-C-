81e6f8f9cda619e5b8f341d6ac0a1d960dce91c5
Luciano Alvarez <140111840+LUXI4NO@users.noreply.github.com>
2025-06-06 21:48:10 -0300

    #9

---
+++ Proyecto Facultad/Facultad/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: luciano
+ * Date: 5/6/2025
+ * Time: 18:17
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Facultad
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
